c9c9b585ca1d4f54f8c9e7e9d83373b83c5928e8
Israel Kenneth <israel.kenneth2011@gmal.com>
2022-10-27 00:11:32 +0200

    Header complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import "./NavbarStyles.css"
+import { Link } from 'react-router-dom'
+import MenuItems from './MenuItems'
+
+const Navbar = () => {
+
+
+    const [isToggled, setIsToggled] = useState(false);
+
+
+
+    const HandleClick = React.useCallback(
+        () => setIsToggled(!isToggled),
+        [isToggled, setIsToggled],
+    );
+
+
+    return (
+
+        <nav className="NavbarItems">
+            <h1 className='navbar-logo'>Trippy</h1>
+            <div className='menu-icons' onClick={HandleClick}>
+
+                <i className={isToggled === false ? "fas fa-times" : 'fas fa-bars'}></i>
+            </div>
+            <ul className={isToggled === false ? "nav-menu active" : "nav-menu"}>
+                {MenuItems.map((items, index) => {
+                    return (<li key={index}>
+                        <a className={items.cName} href={items.url}><i className={items.icon}></i>{items.title}</a>
+                    </li>)
+                })}
+                <button>Signup</button>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
+
+
+{/* <i className='fas fa-times'></i> */ }
